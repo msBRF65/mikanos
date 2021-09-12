226f5a9166d64d86976a5496f2ad6d04c4a3633a
msBRF65 <msaito6525@gmail.com>
2021-09-12 14:19:49 +0900

    add: osbook_day03c

--- kernel/build.sh
+++ kernel/build.sh
@@ -1,7 +1,8 @@
+source /home/makoto/osbook/devenv/buildenv.sh
 rm -f build.log
 touch build.log
-clang++ -O2 -Wall -g --target=x86_64-elf -ffreestanding \
--mno-red-zone -fno-exceptions -fno-rtti -std=c++17 -c main.cpp \
+clang++ $CPPFLAGS -O2 --target=x86_64-elf -ffreestanding \
+-fno-exceptions -c main.cpp \
 > build.log 2>&1
-ld.lld --entry KernelMain -z norelro --image-base 0x100000 \
---static -o kernel.elf main.o > build.log 2>&1
+ld.lld $LDFLAGS --entry KernelMain -z norelro --image-base \
+0x100000 --static -o kernel.elf main.o > build.log 2>&1
