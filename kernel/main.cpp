ba958e42e1ebb992729451112bad3e9ba0313344
msBRF65 <msaito6525@gmail.com>
2021-09-14 20:34:58 +0900

    add: osbook_day05c

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -40,8 +40,11 @@ KernelMain(const FrameBufferConfig &frame_buffer_config)
             pixel_writer->Write(x, y, {0, 255, 0});
         }
     }
-    WriteAscii(*pixel_writer, 50, 50, 'A', {0, 0, 0});
-    WriteAscii(*pixel_writer, 58, 50, 'A', {0, 0, 0});
+    int i = 0;
+    for (char c = '!'; c <= '~'; ++c, ++i)
+    {
+        WriteAscii(*pixel_writer, 8 * i, 50, c, {0, 0, 0});
+    }
     while (1)
         __asm__("hlt");
 }
