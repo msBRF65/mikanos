81c3551c05a208577f5a24a62adb91c782c6a715
msBRF65 <msaito6525@gmail.com>
2021-09-21 19:28:57 +0900

    add: osbook_day09d

--- kernel/main.cpp
+++ kernel/main.cpp
@@ -45,7 +45,14 @@ int printk(const char *format, ...)
     result = vsprintf(s, format, ap);
     va_end(ap);
 
+    StartLAPICTimer();
     console->PutString(s);
+    auto elapsed = LAPICTimerElapsed();
+    StopLAPICTimer();
+
+    sprintf(s, "[%9d]", elapsed);
+    console->PutString(s);
+
     return result;
 }
 
